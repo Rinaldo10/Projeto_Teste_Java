01b3235a2e223951c126ee2868cd9be45df96303
Rinaldo10 <rinaldo.reverte@gmail.com>
2025-03-11 14:16:00 -0400

    first commit

---
+++ src/test/java/my/project/example/userms/UsermsApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.example.userms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
